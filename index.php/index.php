ca4ba0df5fa94ea62086eb2bf1bf4a7fbfc89807
G SUNIL KUMAR <68798513+skuser404@users.noreply.github.com>
2025-04-14 13:22:54 +0530

    Add files via upload

---
+++ index.php/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First PHP Page</title>
+</head>
+<body>
+    <h1>Welcome to PHP, Suni! 🎉</h1>
+    <p>Today's date is: <?php echo date("Y-m-d H:i:s"); ?></p>
+</body>
+</html>
